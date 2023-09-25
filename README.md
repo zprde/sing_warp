3442aecdb3cd8b195cc8402a67756f93541a9a19
Adrian Zhao <zprde@hotmail.com>
2023-09-25 14:00:39 +0800

    the init implementation of integrate warp and singbox

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # sing_warp
 Integrate singbox and cloudflare warp.
 
+## requirements
+- Docker
+- Docker-compose
+
+## How to start
+```shell
+docker-compose up -d sing-warp
+```
+
+Then the `0.0.0.0:2080` can be a http/https/sock5 proxy port.
+
 Reference Repos:
 - [wgcf](https://github.com/ViRb3/wgcf)
 - [sing-box](https://github.com/SagerNet/sing-box)
---
+++ wireguard/config/best.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+PRIVATE_KEY=$(cat config.toml | grep private_key | sed "s/^.*'\(.*\)'$/\1/g")
+echo "private_key: $PRIVATE_KEY"
+echo "" > config.toml
+
+BEST=$(cat result.csv | head -2 | tail -1 | sed -E 's/^(.*?),.*%.*$/\1/g')
+BEST_IP=$(echo $BEST | sed -E 's/^(.*):(.*)$/\1/g')
+BEST_PORT=$(echo $BEST | sed -E 's/^(.*):(.*)$/\2/g')
+
+echo "best endpoint: $BEST_IP:$BEST_PORT"
+
+cat config.tmp.json \
+| sed "s/SERVER_IP/$BEST_IP/g" \
+| sed "s/SERVER_PORT/$BEST_PORT/g" \
+| sed "s/PRIVATE_KEY/$PRIVATE_KEY/g"
+> config.json
+chmod 666 config.json
